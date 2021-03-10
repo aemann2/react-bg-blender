7059fa3d5063ee8989acaa2a49ee44b55f0e7a4a
Adam Mann <aemann2@gmail.com>
2021-03-09 17:23:27 -0800

    completed loader

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -3,20 +3,19 @@ import spinner from '../images/spinner.svg';
 
 function Photo() {
   const [photo, setPhoto] = useState(null);
-  const [loading, setLoading] = useState();
+
+  const fetchPhoto = () => {
+    const url = 'https://picsum.photos/800?grayscale';
+
+    fetch(url)
+      .then((response) => response.blob())
+      .then((data) => {
+        const imgURL = URL.createObjectURL(data);
+        setPhoto(imgURL);
+      });
+  };
 
   useEffect(() => {
-    const fetchPhoto = () => {
-      const url = 'https://picsum.photos/800?grayscale';
-
-      fetch(url)
-        .then((response) => response.blob())
-        .then((data) => {
-          const imgURL = URL.createObjectURL(data);
-          setPhoto(imgURL);
-        });
-    };
-
     fetchPhoto();
   }, []);
 
@@ -24,9 +23,10 @@ function Photo() {
     <div className='img'>
       <div
         className='img__container'
-        style={{ backgroundImage: `url(${photo})` }}
+        style={{
+          backgroundImage: photo ? `url(${photo})` : `url(${spinner})`,
+        }}
       ></div>
-      <img className='img__spinner' src='' alt='' />
     </div>
   );
 }
