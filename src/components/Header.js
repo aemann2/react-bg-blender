15d712516220f1e110b0be047b094ee5e86005d0
Adam Mann <aemann2@gmail.com>
2021-03-12 16:34:24 -0800

    fixed header positioning

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,12 @@ import blender from '../images/blender.svg';
 function Header() {
   return (
     <header className='header'>
-      <img src={blender} alt='' className='header__img' />
-      <h1 className='header__heading'>
-        Background <span className='header--indent'>Blender</span>
-      </h1>
+      <div className='header--wrapper'>
+        <img src={blender} alt='' className='header__img' />
+        <h1 className='header__heading'>
+          Background <span className='header--indent'>Blender</span>
+        </h1>
+      </div>
     </header>
   );
 }
